178bb309b7725b2b22c463b7a75c1bf26efef4ac
Eduardo Reis <104876348+GurgelR@users.noreply.github.com>
2024-04-11 12:59:59 -0300

    update some things

--- correct.py
+++ correct.py
@@ -1,12 +1,24 @@
 import geopandas as gpd
 import pandas as pd
 import numpy as np
-import tkinter.filedialog as fd 
+import tkinter.filedialog as fd
+from shapely.geometry import LineString
+import shapely
 
 class App:
     def __init__(self, correctobj) -> None:
-        self.shp_filepath = self.get_filepath(desc="Select the line shapefile", ftypes=[("shp", ".shp")])
-        self.xl_filepath = self.get_filepath(desc="Select the Excel file of GPR lines", ftypes=[("table", ".xlsx .csv")])
+        self.correctobj = correctobj
+        #self.shp_filepath = self.get_filepath(desc="Select the line shapefile", ftypes=[("shp", ".shp")])
+        #self.xl_filepath = self.get_filepath(desc="Select the Excel file of GPR lines", ftypes=[("table", ".xlsx .csv")])
+        self.shp_filepath = "input/example_gpr_lines.shp"
+        self.xl_filepath = "input/example_excel.xlsx"
+
+        self.line_shp = gpd.read_file(self.shp_filepath)
+        self.line_shp = self.correct_format_df(self.line_shp)
+        self.line_lengs = pd.read_excel(self.xl_filepath)
+        self.line_lengs = self.correct_format_df(self.line_lengs)
+
+        self.correct_lengs()
 
     def get_filepath(self, desc=str, ftypes=list):
         """
@@ -17,19 +29,24 @@ class App:
     
     def export_new_shp(self):
         pass
+
+    def correct_format_df(self, df):
+        df.columns = [column.lower() for column in df.columns]
+        return df
+    
+    def correct_lengs(self):
+        testdf = pd.merge(self.line_shp, self.line_lengs, how='inner', on="line")
+        testdf["shp_len"] = np.round(testdf["geometry"].apply(shapely.length), 2)
+        testdf = testdf.sort_values(by="line").reset_index(drop=True)
+        print (testdf)
         
 class Correct:
     def __init__(self) -> None:
         pass
-
-    def read_shp(self,shp_filepath):
-        return gpd.read_file(shp_filepath)
-
-    def read_xl(self,filepath):
-        return pd.read_excel(filepath)
     
-
-
 if __name__ == "__main__":
     correctobj = Correct()
     app = App(correctobj)
+
+    #print (app.line_lengs)
+    #print (app.line_shp)
