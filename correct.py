d20c7fe09359ff8f1c94724a9258ee8878436f48
Eduardo Reis <104876348+GurgelR@users.noreply.github.com>
2024-04-12 17:00:39 -0300

    Update correct.py

--- correct.py
+++ correct.py
@@ -6,6 +6,9 @@ from shapely.geometry import LineString
 import shapely
 
 class App:
+    """
+    Designed for, in some future, create a GUI.
+    """
     def __init__(self, correctobj) -> None:
         self.correctobj = correctobj
         #self.shp_filepath = self.get_filepath(desc="Select the line shapefile", ftypes=[("shp", ".shp")])
@@ -31,6 +34,9 @@ class App:
         pass
         
 class Correct:
+    """
+    Operations for correcting the shape lengths.
+    """
     def __init__(self) -> None:
         pass
 
@@ -40,22 +46,33 @@ class Correct:
         """
         df.columns = [column.lower() for column in df.columns]
         return df
-    
-    def check_lens(self, line=LineString) -> gpd.GeoDataFrame:
+
+    def expand_line(self, expand_df, line_shp):
         """
-        Function to use with 
+        Operation for expanding the line, if it is smaller then the GPR section
         """
         pass
-    
+
+    def reduce_line(self, reduce_df, line_shp):
+        """
+        Operation for reducing the line, if it is bigger then the GPR section 
+        """
+        pass
+
+    def check_lens(self, df):
+
+        print (df["line"])
+
+        pass
+
     def correct_lengs(self, line_shp=gpd.GeoDataFrame, line_lengs=pd.DataFrame):
         
         testdf = pd.merge(line_shp, line_lengs, how='inner', on="line")
         testdf["or_shp_len"] = np.round(testdf["geometry"].apply(shapely.length), 2)
         testdf = testdf.sort_values(by="line").reset_index(drop=True)
-        print (testdf)
-
         
-    
+        testdf.apply(self.check_lens)
+
 if __name__ == "__main__":
     correctobj = Correct()
     app = App(correctobj)
