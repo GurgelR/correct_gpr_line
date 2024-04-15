52276acfe342a1101ebf485c0d3a6da698c6791d
Eduardo Reis <104876348+GurgelR@users.noreply.github.com>
2024-04-15 12:00:58 -0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -5,3 +5,12 @@ lines or whatsoever within the main window.
 
 It will call the functions created and implemented in "correct.py"
 """
+
+import correct
+
+class App:
+    def __init__(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    App()
